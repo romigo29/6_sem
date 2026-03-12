cbce8b9d3f5eb0fac2e1d119725cf0eb55b58b48
Igor <152262528+romigo29@users.noreply.github.com>
2026-03-12 21:35:35 +0300

    trvp lab3 start

---
+++ ТРВП/Lab01_NGINX/nginx/static/TDWA03-01.js
@@ -0,0 +1,52 @@
+const resultEl = document.getElementById("result");
+
+async function handleFetch(method, data = null) {
+    let url = "https://localhost:20443/api/Save-JSON";
+
+    let options = { method };
+
+    if (data) {
+        options.headers = { "Content-Type": "application/json" };
+        options.body = JSON.stringify(data);
+    }
+
+    try {
+        const response = await fetch(url, options);
+
+
+        const text = await response.text();
+        let json;
+        try {
+            json = text ? JSON.parse(text) : {};
+        } catch {
+            json = { error: text };
+        }
+
+        resultEl.textContent = JSON.stringify(json, null, 2);
+    } catch (err) {
+        resultEl.textContent = "Ошибка: " + err;
+    }
+}
+
+
+document.getElementById("btnGet").addEventListener("click", () => handleFetch("GET"));
+
+document.getElementById("btnPost").addEventListener("click", () => {
+    const data = {
+        op: document.getElementById("op").value,
+        x: Number(document.getElementById("x").value),
+        y: Number(document.getElementById("y").value)
+    };
+    handleFetch("POST", data);
+});
+
+document.getElementById("btnPut").addEventListener("click", () => {
+    const data = {
+        op: document.getElementById("op").value,
+        x: Number(document.getElementById("x").value),
+        y: Number(document.getElementById("y").value)
+    };
+    handleFetch("PUT", data);
+});
+
+document.getElementById("btnDelete").addEventListener("click", () => handleFetch("DELETE"));
